992c5a4f61fd068bb23453bb4fbc45df1c04a890
Maximilian Schneider <mail@maximilianschneider.net>
2013-09-28 00:33:20 +0200

    initial commit

---
+++ public/canvasFocus.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+  // while focussed, play sketch
+  $("canvas").focus(function(){
+    //window.console.log('focus on '+this.id);
+    var sketch = Processing.getInstanceById(this.id);
+    if (sketch) {
+      if(sketch.loop) sketch.loop();
+      if(sketch.unmute) sketch.unmute();
+    }
+  });
+  
+  // when focus is lost, pause sketch
+  $("canvas").blur(function(){
+    //window.console.log('focus lost by '+this.id);
+    var sketch = Processing.getInstanceById(this.id);
+    if (sketch) {
+      if(sketch.noLoop) sketch.noLoop();
+      if(sketch.mute) sketch.mute();
+    }
+  });
+});
