c15a8b96872d8bb4f6aeef63386ad469ef23757c
Maximilian Schneider <mail@maximilianschneider.net>
2013-09-28 02:46:53 +0200

    game is now wired to afterlife via socket.io

--- web.js
+++ web.js
@@ -5,6 +5,13 @@ app.use(express.static('public'));
 app.use(app.router);
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+var socket = app.listen(port, function() {
+    console.log("Listening for http on " + port);
+});
+
+var io = require('socket.io').listen(socket);
+io.sockets.on('connection', function (socket) {
+  socket.on('mario_died', function (data) {
+    io.sockets.emit('add_mario', data);
+  });
 });
